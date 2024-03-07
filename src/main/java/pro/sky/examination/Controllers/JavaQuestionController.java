749c9512571540789d9daaf6636442c2203d0787
IMBor1 <ilacerber1@gmail.com>
2024-03-07 21:07:13 +0300

    Task2. @Qualifier не отрабатывает. И не работает пункт 4.

--- src/main/java/pro/sky/examination/Controllers/JavaQuestionController.java
+++ src/main/java/pro/sky/examination/Controllers/JavaQuestionController.java
@@ -1,5 +1,6 @@
 package pro.sky.examination.Controllers;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,7 @@ import java.util.Collection;
 
 
 @RestController
+@Qualifier
 @RequestMapping(path = "/exam/java")
 public class JavaQuestionController {
     private final JavaQuestionServiceImpl javaQuestionService;
